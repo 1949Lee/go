dead59e7ab78706e201db9915c5111b0da190028
lijiaxuan <lijiaxuan@lijiaxuandeMacBook-Pro.local>
2018-06-15 15:34:04 +0800

    变量，常量

--- goLearning/hello.go
+++ goLearning/hello.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+var x, y int
+var ( // 这种因式分解关键字的写法一般用于声明全局变量
+	a int
+	b bool
+)
+
+var c, d int = 1, 2
+var e, f = 123, "hello"
+
+//这种不带声明格式的只能在函数体中出现
+//g, h := 123, "hello"
+//g,h = 123,hello
+
 func main() {
-	fmt.Print("Hello World\n")
+	a = 1
+	b = true
+	fmt.Println("Hello World")
+	fmt.Println("Hello World")
 }
