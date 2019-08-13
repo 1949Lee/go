8e340c5eca6dbb6657e32f0420585038579e7645
lijiaxuan <lijiaxuan0811@hotmail.com>
2019-08-13 17:39:05 +0800

    update

--- leeBlogCli/test/handler/handler.go
+++ leeBlogCli/test/handler/handler.go
@@ -1,7 +1,55 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ParamNewArticle struct {
+	Text string
+}
+type ResponseResult struct {
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+}
 
 func ReadMarkdownText(writer http.ResponseWriter, r *http.Request) {
 
+	/**
+	  此部分代码可以提取出来。*/
+	//设置跨域的相应头CORS，CORS参考：https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Access_control_CORS
+	writer.Header().Add("Access-Control-Allow-Origin", "http://localhost:63342")
+	writer.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	writer.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	// 请求方发送请求时当请求为options时，直接返回200。
+	if r.Method == "OPTIONS" {
+		writer.WriteHeader(200)
+		return
+	}
+
+	//下面是接口真正的处理
+	var param ParamNewArticle
+	result := ResponseResult{}
+	body, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	err := json.Unmarshal(body, &param)
+	if err != nil {
+		writer.Write([]byte("error params"))
+	}
+	writer.Header().Add("Access-Control-Allow-Credentials", "true")
+	result.Code = 0
+	result.Data = struct {
+		Text string   `json:"text"`
+		List []string `json:"list"`
+	}{
+		Text: "success",
+		List: []string{"a", "b", "c"},
+	}
+	b, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	writer.Write([]byte(b))
 }
--- leeBlogCli/test/server.go
+++ leeBlogCli/test/server.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"leeBlogCli/test/handler"
 	"net/http"
-	"os"
 )
 
 func main() {
-
+	port := "1314"
 	http.HandleFunc("/", handler.ReadMarkdownText)
-
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("server start with http://localhost:%s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
-	if file, err := os.Open("./test/test.md"); err != nil {
-		panic(err)
-	} else {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-	}
+	//if file, err := os.Open("./test/test.md"); err != nil {
+	//	panic(err)
+	//} else {
+	//	scanner := bufio.NewScanner(file)
+	//	for scanner.Scan() {
+	//		fmt.Println(scanner.Text())
+	//	}
+	//}
 }
