f35c4539a52d6a6b9987f1bf8e55d8817cfb805a
1949Lee <lijiaxuan0811@hotmail.com>
2019-11-03 22:01:13 +0800

    websocket完成

--- leeBlogCli/test/handler/handler.go
+++ leeBlogCli/test/handler/handler.go
@@ -3,11 +3,11 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/websocket"
 	"io/ioutil"
 	"leeBlogCli/test/parser"
-    "log"
-    "net"
-    "net/http"
+	"log"
+	"net/http"
 	"time"
 )
 
@@ -20,12 +20,23 @@ type ResponseResult struct {
 	Data interface{} `json:"data"`
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		if r.Header.Get("Origin") == "http://localhost:3000" {
+			return true
+		}
+		return false
+	},
+}
+
 func ReadMarkdownText(writer http.ResponseWriter, r *http.Request) {
 
 	/**
 	  此部分代码可以提取出来。*/
 	//设置跨域的相应头CORS，CORS参考：https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Access_control_CORS
-	writer.Header().Add("Access-Control-Allow-Origin", "http://localhost:63342")
+	writer.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
 	writer.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	writer.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	// 请求方发送请求时当请求为options时，直接返回200。
@@ -64,10 +75,47 @@ func ReadMarkdownText(writer http.ResponseWriter, r *http.Request) {
 	fmt.Println("app elapsed:", time.Since(t))
 }
 
+func SocketReadMarkdownText(writer http.ResponseWriter, r *http.Request) {
+	//header := http.Header{}
+	//header.Add("Access-Control-Allow-Origin", "http://localhost:3000")
+	conn, err := upgrader.Upgrade(writer, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+	for {
+		var param ParamNewArticle
+		result := ResponseResult{}
+		err := conn.ReadJSON(&param)
 
-func SocketReadMarkdownText(conn net.Conn) {
-    _, err := conn.Write([]byte("hello"))
-    if err!=nil {
-        log.Printf( "tcp write error:%v",err)
-    }
+		if err != nil {
+			//if err = {
+			//    break
+			//}
+			_, ok := err.(*websocket.CloseError)
+			if ok {
+				break
+			}
+			log.Printf("receive err:%v", err)
+			continue
+		}
+		result.Code = 0
+
+		dataList, html := parser.MarkdownParse(param.Text)
+		result.Data = struct {
+			Text         string              `json:"text"`
+			List         []parser.TokenSlice `json:"list"`
+			MarkDownHtml string              `json:"html"`
+		}{
+			Text:         "success",
+			List:         dataList,
+			MarkDownHtml: html,
+		}
+		if err = conn.WriteJSON(result); err != nil {
+			log.Printf("receive err:%v", err)
+			break
+		}
+	}
+
 }
--- leeBlogCli/test/server.go
+++ leeBlogCli/test/server.go
@@ -3,33 +3,32 @@ package main
 import (
 	"fmt"
 	"leeBlogCli/test/handler"
-    "log"
-    "net"
-    "net/http"
+	"net/http"
 	_ "net/http/pprof"
 )
 
 func main() {
 	port := "1314"
-	http.HandleFunc("/", handler.ReadMarkdownText)
+	http.HandleFunc("/once", handler.ReadMarkdownText)
+	http.HandleFunc("/ws", handler.SocketReadMarkdownText)
 	fmt.Printf("server start with http://localhost:%s\n", port)
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
-    listener, err := net.Listen("tcp", ":1315")
-    if err!=nil{
-        panic(err)
-    }
-    for {
-        conn, err := listener.Accept()
-        if err!=nil{
-            log.Printf("accept error:%v",err)
-            continue
-        }
-
-        go func(conn net.Conn) {
-            handler.SocketReadMarkdownText(conn)
-        }(conn)
-    }
+	//listener, err := net.Listen("tcp", ":1315")
+	//if err!=nil{
+	//    panic(err)
+	//}
+	//for {
+	//    conn, err := listener.Accept()
+	//    if err!=nil{
+	//        log.Printf("accept error:%v",err)
+	//        continue
+	//    }
+	//
+	//    go func(conn net.Conn) {
+	//        handler.SocketReadMarkdownText(conn)
+	//    }(conn)
+	//}
 }
