c0453b990a28faf33260d24d0fe5012f7aa767fc
lijiaxuan <lijiaxuan0811@hotmail.com>
2019-12-17 14:04:55 +0800

    dao添加

--- leeBlogCli/concurrent/fileServer.go
+++ leeBlogCli/concurrent/fileServer.go
@@ -95,8 +95,8 @@ type FileInfo struct {
 type FileMap map[uint32]*FileInfo
 
 // TODO 上传的图片需要处理，渐进式显示图片，图片压缩。https://godoc.org/gopkg.in/gographics/imagick.v2/imagick、https://github.com/gographics/imagick、https://godoc.org/gopkg.in/gographics/imagick.v2/imagick、https://godoc.org/gopkg.in/gographics/imagick.v3/imagick
-// TODO 前两个字节（16位）表示每个文件的第几个分片（最大共65536个分片）；后两个字节表示每个分片的大小（单位kb，默认64kb）。所以单个文件最大64kb * 2^16=4G
 
+// 前两个字节（16位）表示每个文件的第几个分片（最大共65536个分片）；后两个字节表示每个分片的大小（单位kb，默认64kb）。所以单个文件最大64kb * 2^16=4G
 // 文件服务器：上传下载相关。
 type FileServer struct {
 	// 处理中的文件映射
