240feb94068f016f1878c1265f2fb46edfe88b5e
1949Lee <lijiaxuan0811@hotmail.com>
2020-01-10 00:03:57 +0800

    bugfix

--- leeBlogCli/dao/daoArticle.go
+++ leeBlogCli/dao/daoArticle.go
@@ -84,7 +84,61 @@ func (s *DBServer) InsertArticle(param *definition.SaveArticleInfo) bool {
 
 // 根据传入参数，更新对应文章
 func (s *DBServer) UpdateArticle(param *definition.SaveArticleInfo) bool {
-	//if
+	//var summary string
+	//if len(param.Text) >= config.SummaryLength {
+	//    summary = param.Text[0:config.SummaryLength]
+	//} else {
+	//    summary = param.Text
+	//}
+	//
+	//tx := s.DB.MustBegin()
+	//defer tx.Rollback()
+	//_, err := tx.Exec(`UPDATE article set article_ctg=?,article_title=?,article_summary=?,article_content=?,article_updatetime=?;`,
+	//    param.ArticleHeader.Category.ID,
+	//    param.ArticleHeader.Title,
+	//    summary, // 文章摘要
+	//    param.Content,
+	//    param.ArticleHeader.UpdateTime,
+	//)
+	//if err != nil {
+	//    _ = tx.Rollback()
+	//    log.Printf("dao.InsertArticle 插入文章报错errror:%v", err)
+	//    return false
+	//}
+	//
+	//relationSQL := strings.Builder{}
+	//relationSQL.WriteString(`INSERT INTO articles_tags_relation (relation_article, relation_tag) VALUES`)
+	//valueSQL := make([]interface{}, 0)
+	//for i, tag := range param.ArticleHeader.Tags {
+	//    if i < len(param.ArticleHeader.Tags)-1 {
+	//        relationSQL.WriteString(`(?,?),`)
+	//    } else {
+	//        relationSQL.WriteString(`(?,?);`)
+	//    }
+	//    valueSQL = append(valueSQL, param.ArticleHeader.ID, tag.ID)
+	//}
+	//
+	//txStmt, err := tx.Prepare(relationSQL.String())
+	//if err != nil {
+	//    _ = tx.Rollback()
+	//    log.Printf("dao.InsertArticle 插入文章和标签关系SQL预编译报错errror:%v", err)
+	//    return false
+	//}
+	//defer txStmt.Close()
+	//_, err = txStmt.Exec(valueSQL...)
+	//if err != nil {
+	//    _ = tx.Rollback()
+	//    log.Printf("dao.InsertArticle 插入文章和标签关系报错errror:%v", err)
+	//    return false
+	//}
+	//
+	//err = tx.Commit()
+	//if err != nil {
+	//    log.Printf("dao.InsertArticle 报错errror:%v", err)
+	//    return false
+	//} else {
+	//    return true
+	//}
 	return true
 }
 
